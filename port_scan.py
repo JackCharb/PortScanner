88cb15358bcf5ed6cef1dacf277f2b71e4a18694
JackCharb <jlcharbonneau@wpi.edu>
2018-09-17 20:48:46 -0400

    scan all ports by default

--- port_scan.py
+++ port_scan.py
@@ -3,53 +3,65 @@
 # Jack Charbonneau 10/14/2017
 
 import argparse
-from socket import *
+import socket
 
-parser = argparse.ArgumentParser()
-
-parser.add_argument('-H', type=str, dest='tgtHost', help='specify target host')
-parser.add_argument('-p', type=str, dest='tgtPorts', help='specify target ports separated by commas')
-
-args = parser.parse_args()
-tgtHost = args.tgtHost
-tgtPorts = args.tgtPorts
 
 def main():
-    if (tgtHost is None) | (tgtPorts[0] is None):
-        print(parser.usage)
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-H', type=str, dest='tgtHost', help='specify target host')
+    parser.add_argument('-p', type=str, dest='tgtPorts', help='specify target ports separated by commas')
+
+    args = parser.parse_args()
+    tgtHost = args.tgtHost
+    tgtPorts = args.tgtPorts
+
+    if (tgtHost is None):
+        print(parser.usage)  # write parser usage
         exit(0)
     else:
+        if tgtPorts is None:
+            tgtPorts = list(range(1, 65536))
+        else:
+            tgtPorts = args.tgtPorts.split(",")
         portScan(tgtHost, tgtPorts)
 
 
 def portScan(tgtHost, tgtPorts):
     try:
-        tgtIP = gethostbyname(gtgHost) # use get address info instead
-    except:
-        print("[-] Cannot resolve '%s': Unknown Host" % tgtHost)
+        tgtIP = socket.gethostbyname(tgtHost)  # use get address info instead
+    except socket.gaierror:
+        print("[-] Could not resolve hostname: '%s'" % tgtHost)
         return
+    except socket.error:
+        print("[-] Could not connect to server")
+        return
+
     try:
-        tgtName = gethostbyaddr(tgtIP)
+        tgtName = socket.gethostbyaddr(tgtIP) # fix this
         print("[-] Scan results for: " + tgtName[0])
     except:
         print("[-] Scan results for: " + tgtIP)
 
-    setdefaulttimeout(1)
+    socket.setdefaulttimeout(1)
 
     for tgtPort in tgtPorts:
-        print("Scanning Port " + tgtPort)
         connScan(tgtHost, int(tgtPort))
 
 
 def connScan(tgtHost, tgtPort):
     try:
-        connSkt = socket(AF_INET, SOCK_STREAM)
-        connSkt.connect((tgtHost, tgtPort))
-        print("[+] %d tcp open" % tgtPort)
-        connSkt.close()
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((tgtHost, tgtPort))
+        print("[+] Port %d open" % tgtPort)
+        sock.close()
+
+    except KeyboardInterrupt:
+        print()
+        exit()
 
     except:
-        print("[-] %d tcp closed" % tgtPort)
+        print("[-] Port %d closed" % tgtPort)
 
 if __name__ == "__main__":
     main()
